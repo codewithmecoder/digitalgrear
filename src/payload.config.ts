91f1bc74ae4518ba51c8715197ef2bbfcb952645
codewithmecoder <yaty1031@gmail.com>
2024-05-27 22:10:30 +0700

    add payload users collecttion

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,16 +1,25 @@
+import dotenv from "dotenv";
 import { viteBundler } from "@payloadcms/bundler-vite";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import path from "path";
 import { buildConfig } from "payload/config";
+import { Users } from "./collections/User";
+
+console.log(path.resolve(__dirname, "../.env"));
+
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+});
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [],
+  collections: [Users],
   routes: {
     admin: "/sell",
   },
   admin: {
+    user: "users",
     bundler: viteBundler(),
     meta: {
       titleSuffix: "- DigitalGear",
